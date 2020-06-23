771e28afe15e24cd48627064212623d561c238ea
Ken <a01056715@gmail.com>
2020-06-23 04:24:08 -0700

    added pointers

--- init/main.go
+++ init/main.go
@@ -2,12 +2,15 @@ package main
 
 import "fmt"
 
+// This gets called first
 var WhatIsThe = AnswerToLife()
 
+// WhatIsThe is initialized to 42
 func AnswerToLife() int {
 	return 42
 }
 
+// Init then gets called which reassigns 0
 func init() {
 	WhatIsThe = 0
 }
---
+++ pointers/example1/retirement.go
@@ -0,0 +1,7 @@
+package example1
+
+import "fmt"
+
+func YearsUntilRetirement(age *int) {
+	fmt.Println(100 - *age)
+}
